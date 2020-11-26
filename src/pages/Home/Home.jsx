eabdc5ff94b767d935e45b4532ad2fcd4b90a8d8
FloLeh <florianlehuede@gmail.com>
2020-11-26 13:09:22 +0100

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './Home.css'
 import logo_big_catch from '../../assets/images/logo_big_catch.png'
+import shadow1 from '../../assets/images/shadow1.png'
 
 export class Home extends Component {
   render() {
     return (
       <div className='home'>
-        <img src={logo_big_catch} alt="la planque"/>
+        <img className='home__shadow' src={shadow1} alt="shadow"/>
+        <img className='home__logo' src={logo_big_catch} alt="la planque"/>
       </div>
     )
   }
