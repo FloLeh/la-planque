31280971529637b6d2da6724d60d38bb1ca0b80f
FloLeh <florianlehuede@gmail.com>
2020-12-03 16:49:00 +0100

    try some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,18 @@ import { Header, ScrollToTop } from './components'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import React, { Component } from 'react'
 import { Missions, Home, Projets, Collectif, Contact, SingleProjectPage } from './pages'
+import $ from 'jquery'
 
 class App extends Component {
+
+  resizeBackground() {
+    $('body').height($(window).height() + 60);
+  }
+
+  componentDidMount() {
+    $(window).on('resize', () => this.resizeBackground())
+  }
+
   render () {
     return (
       <div className="App">
