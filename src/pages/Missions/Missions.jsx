cf7e9b40de7ec4303d5b0c2a26bdc23be5ed643e
FloLeh <florianlehuede@gmail.com>
2020-12-04 17:08:37 +0100

    mission component

--- src/pages/Missions/Missions.jsx
+++ src/pages/Missions/Missions.jsx
@@ -6,6 +6,7 @@ import mobilier from '../../assets/images/picto_archi_int.png'
 import visuel from '../../assets/images/picto_3d.png'
 import shadow2 from '../../assets/images/shadow2.png'
 import './Missions.css'
+import { Mission } from '../../components'
 
 export class Missions extends Component {
   render() {
@@ -13,26 +14,11 @@ export class Missions extends Component {
       <div id='missions' className='missions'>
         <img className='mission__shadow' src={shadow2} alt="shadow2"/>
         <div className="pictos">
-          <div className="mission">
-            <img src={paysage} alt="paysage" data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay='100'/>
-            <div>PAYSAGE <br/> </div>
-          </div>
-          <div className="mission">
-            <img src={dossier} alt="dossier" data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay='200'/>
-            <div>DÉMARCHES ADMINISTRATIVES</div>
-          </div>
-          <div className="mission">
-            <img src={archi} alt="archi" data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay='300'/>
-            <div>ARCHITECTURE <br/> </div>
-          </div>
-          <div className="mission">
-            <img src={mobilier} alt="mobilier" data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay='400'/>
-            <div>MOBILIER SUR MESURE</div>
-          </div>
-          <div className="mission">
-            <img src={visuel} alt="visuel" data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay='500'/>
-            <div>VISUELS 3D</div>
-          </div>
+          <Mission text='PAYSAGE' alt='paysage' delay='100' image={paysage}/>
+          <Mission text='DÉMARCHES ADMINISTRATIVES' alt='dossier' delay='200' image={dossier}/>
+          <Mission text='ARCHITECTURE' alt='archi' delay='300' image={archi}/>
+          <Mission text='MOBILIER SUR MESURE' alt='mobilier' delay='400' image={mobilier}/>
+          <Mission text='VISUELS 3D' alt='visuels' delay='500' image={visuel}/>
         </div>
         <div className='mission__bottom'>Le collectif vous propose des missions adaptées à votre projet et à vos besoins. <br/> Un projet peut être traité dans sa globalité ou partiellement.</div>
       </div>
