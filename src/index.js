788cd269166dd647a8c77a9e03166fc5fca49fa1
FloLeh <florianlehuede@gmail.com>
2020-12-04 10:12:10 +0100

    fix mobile issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import $ from 'jquery'
+
+$(window).on('resize', () => $('body').height($(window).height() + 60))
 
 ReactDOM.render(
   <React.StrictMode>
