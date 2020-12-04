61792be597b0a2e625b0548a7327497d3da6bb06
FloLeh <florianlehuede@gmail.com>
2020-12-04 11:08:22 +0100

    fix mobile issues

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
