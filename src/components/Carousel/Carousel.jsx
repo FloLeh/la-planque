fae77c0ef6e59018c4adb960db904e558afc1221
FloLeh <florianlehuede@gmail.com>
2020-12-04 15:58:17 +0100

    options for flickity

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Flickity from "react-flickity-component"
 import './Carousel.css'
-
 import 'flickity/css/flickity.css'
+
+const flickityOptions = {
+  wrapAround: true
+}
 
 export default function Carousel() {
   return (
+    
     <div className='carousel'>
-      <Flickity >
+      <Flickity options={flickityOptions}>
         <img src="https://loremflickr.com/800/480" alt='' />
         <img src="https://loremflickr.com/800/480/kittens" alt='' />
         <img src="https://loremflickr.com/800/480/kitten" alt='' />
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './Menu.css'
 import logo from '../../assets/images/logo_small.png'
-import { Link } from 'react-router-dom'
 
 export class Menu extends Component {
   componentDidMount() {
@@ -23,9 +22,9 @@ export class Menu extends Component {
           <ul>
             <li><a href="#missions">MISSIONS</a><span></span></li>
             <li><a href="#projets">PROJETS</a><span></span></li>
-            <li><Link to="/">
+            <li>
               <img className='menu__logo' src={logo} alt='logo'></img>
-            </Link></li>
+            </li>
             <li><a href="#collectif">COLLECTIF</a><span></span></li>
             <li><a href="#contact">SE RENCONTRER</a><span></span></li>
           </ul>
