c883f32db19e05685b32424922339de849fa27fb
FloLeh <florianlehuede@gmail.com>
2020-12-07 10:21:29 +0100

    refresh anims on logo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,13 +6,30 @@ import la_big from '../../assets/images/LA_big.png'
 import shadow1 from '../../assets/images/shadow1.png'
 
 export class Home extends Component {
+
+  refreshAnims() {
+    window.onscroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        document.querySelector('.home__la').classList.remove('la-animation')
+        document.querySelector('.home__catch').classList.remove('catch-animation')
+      } else {
+        document.querySelector('.home__la').classList.add('la-animation')
+        document.querySelector('.home__catch').classList.add('catch-animation')
+      }
+    }
+  }
+
+  componentDidMount() {
+    this.refreshAnims()
+  }
+  
   render() {
     return (
       <div id='home' className='home'>
         <img className='home__shadow' src={shadow1} alt="shadow"/>
         <img className='home__logo' src={logo_big_empty} alt="planque"/>
-        <img className='home__la' src={la_big} alt="la"/>
-        <img className='home__catch' src={catch_phrase} alt="catch"/>
+        <img className='home__la la-animation' src={la_big} alt="la"/>
+        <img className='home__catch catch-animation' src={catch_phrase} alt="catch"/>
       </div>
     )
   }
