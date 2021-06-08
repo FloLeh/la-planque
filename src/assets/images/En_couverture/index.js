d3a53d58e331308b0d45d7e49a87d57dea571da7
Florian <florianlehuede@gmail.com>
2021-06-08 14:32:34 +0200

    reverse visuels

--- src/assets/images/En_couverture/index.js
+++ src/assets/images/En_couverture/index.js
@@ -1,4 +1,4 @@
 export { default as ARCHITECTURE } from './Couverture_architecture.jpg'
 export { default as MOBILIER } from './Couverture_mobilier.jpg'
 export { default as PAYSAGE } from './Couverture_paysage.jpg'
-export { default as VISUELS_3D } from './Couverture_paysage.jpg'
+export { default as VISUELS_3D } from './Couverture_visuel_3D.jpg'
