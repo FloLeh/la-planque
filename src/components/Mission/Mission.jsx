1410c2d0af5d0902aabf9d7193041cfe9dbac8ea
Florian <florianlehuede@gmail.com>
2021-06-21 22:11:38 +0200

    fixes safari

--- src/components/Mission/Mission.jsx
+++ src/components/Mission/Mission.jsx
@@ -4,7 +4,9 @@ import './Mission.css'
 export default function Mission(props) {
   return (
     <div className="mission">
-      <img src={props.image} alt={props.alt} data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay={window.innerWidth > 640 ? props.delay : '100'}/>
+      <div className="mission_img">
+        <img src={props.image} alt={props.alt} data-aos-duration='800' data-aos='fade-up' data-aos-anchor-placement="top-center" data-aos-offset='-150' data-aos-delay={window.innerWidth > 640 ? props.delay : '100'}/>
+      </div>
       <div>{props.text}</div>
     </div>
   )
